a980a7ac3c4e6ded149795a9a393e8314d86a788
Oded Arbel <oded@geek.co.il>
2017-07-17 19:21:57 +0300

    streamline installation and add change author plugin

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,18 +1,24 @@
 #!/bin/bash -ex
 
+function install_from_github() {
+	local repo="$1" bundler="$2"
+	read username reponame <<<"${repo/\// }"
+	[ -d "$reponame" ] && return 0
+	(
+		curl -L https://api.github.com/repos/"$repo"/tarball | tar -zx --xform="s,$username-$reponame-[[:alnum:]]*,$reponame,"
+		cd "$reponame"
+		[ -n "$bundler" ] && rm -f Gemfile.lock && bundle
+	)
+}
+
 cd /redmine/plugins
 
-[ -d "redmine_omniauth_google" ] || (
-	curl -L https://github.com/twinslash/redmine_omniauth_google/archive/master.tar.gz | tar -zx --xform=s,redmine_omniauth_google-master,redmine_omniauth_google,
-	cd redmine_omniauth_google
-	rm Gemfile.lock
-	bundle
-)
+install_from_github twinslash/redmine_omniauth_google yes
 
-[ -d "redmine_git_remote" ] || (
-	git clone https://github.com/dergachev/redmine_git_remote
-	mkdir redmine_git_remote/repos
-	chown redmine:redmine redmine_git_remote/repos
-)
+install_from_github dergachev/redmine_git_remote
+mkdir redmine_git_remote/repos
+chown redmine:redmine redmine_git_remote/repos
+
+install_from_github woblavobla/redmine_changeauthor
 
 touch /redmine/files/plugins-are-ready
