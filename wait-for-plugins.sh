aa3c241d60abcf69db20ac02e72ea7832781ee6d
Oded Arbel <oded@geek.co.il>
2017-08-13 16:56:18 +0300

    make sure that the plugins are installed correctly

--- wait-for-plugins.sh
+++ wait-for-plugins.sh
@@ -7,4 +7,11 @@ done
 
 cp /usr/src/redmine/config/configuration.yml{.example,}
 
+apt-get update && apt-get install -qy make gcc
+
+for dir in /usr/src/redmine/plugins/*; do
+        [ -d "$dir" ] || continue
+        (cd "$dir"; bundle)
+done
+
 exec /docker-entrypoint.sh "$@"
