80194ba0ba46236202280a1795b8ce73b630bbb9
Oded Arbel <oded@geek.co.il>
2017-08-13 17:42:23 +0300

    dont copy example over, now that we have a configuration in the repo

--- wait-for-plugins.sh
+++ wait-for-plugins.sh
@@ -5,8 +5,6 @@ while ! [ -f "/usr/src/redmine/files/plugins-are-ready" ]; do
 	sleep 1
 done
 
-cp /usr/src/redmine/config/configuration.yml{.example,}
-
 apt-get update && apt-get install -qy make gcc
 
 for dir in /usr/src/redmine/plugins/*; do
