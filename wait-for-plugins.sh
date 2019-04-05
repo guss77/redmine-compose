859655d8aeb83901392a18811f54c4a9db04cf66
Oded Arbel <oded@geek.co.il>
2019-04-05 16:47:44 +0300

    try to get apt to work, in the face of network timeouts

--- wait-for-plugins.sh
+++ wait-for-plugins.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
 echo "Waiting for plugins to be ready..."
 while ! [ -f "/usr/src/redmine/files/plugins-are-ready" ]; do
 	sleep 1
 done
 
-apt-get update && apt-get install -qy make gcc
+while ! apt-get update; do sleep 5; done
+apt-get install -qy make gcc
 
 for dir in /usr/src/redmine/plugins/*; do
         [ -d "$dir" ] || continue
